14cd24262c2c5a7b6fc2b2079add7386871cbfc1
copiousfreetime <jeremy@copiousfreetime.org>
2023-12-01 13:50:54 -0700

    2023-01

---
+++ 2023/01/README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2023, day 01
+
+<https://adventofcode.com/2023/day/1>
---
+++ 2023/01/answer.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+require 'optparse'
+require 'debug'
+
+options = {}
+OptionParser.new do |parser|
+  parser.banner = "Usage: answer.rb [options]"
+
+  parser.on("-w", "--[no-]words", "Parse Words") do |w|
+    options[:words] = w
+  end
+end.parse!
+
+digit_tokens = ('0'..'9').map(&:to_s)
+word_tokens = %w[
+    zero
+    one
+    two
+    three
+    four
+    five
+    six
+    seven
+    eight
+    nine
+]
+
+token_value_map = {}.tap do |h|
+  [ digit_tokens, word_tokens ].each do |token_list|
+    token_list.each.with_index do |token, value|
+      h[token] = value
+    end
+  end
+end
+
+regex_tokens = digit_tokens
+
+if options[:words]
+  regex_tokens.concat(word_tokens)
+end
+
+reverse_tokens = regex_tokens.map(&:reverse)
+
+forward_regex = Regexp.new("(#{regex_tokens.join("|")})")
+reverse_regex = Regexp.new("(#{reverse_tokens.join("|")})")
+
+values = []
+ARGF.each_line do |line|
+  line.strip!
+
+  forward_token = forward_regex.match(line).captures.first
+  forward_value = token_value_map[forward_token]
+
+  reverse_token = reverse_regex.match(line.reverse).captures.first
+  reverse_value = token_value_map[reverse_token.reverse]
+
+  value           = "#{forward_value}#{reverse_value}".to_i
+  values << value
+end
+puts values.sum
+
