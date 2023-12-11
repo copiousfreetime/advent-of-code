e9dfe8bef2f22ed46c891daec824952c3f76e532
copiousfreetime <jeremy@copiousfreetime.org>
2023-12-10 22:51:27 -0700

    expansion done over transposition

--- 2023/11/answer.rb
+++ 2023/11/answer.rb
@@ -8,6 +8,30 @@ gemfile do
   gem 'tty-cursor'
 end
 
+class Tile
+  attr_reader :row
+  attr_reader :col
+  attr_accessor :id
+
+  def initialize(row:, col:, id: nil)
+    @row = row
+    @col = col
+    @id = id
+  end
+
+  def galaxy?
+    !id.nil?
+  end
+
+  def space?
+    !galaxy?
+  end
+
+  def to_s
+    galaxy? ? "#" : "."
+  end
+end
+
 class Cosmos
   attr_reader :initial_row_count
   attr_reader :initial_col_count
@@ -16,14 +40,29 @@ class Cosmos
     @initial_row_count = row_count
     @initial_col_count = col_count
     @grid = Array.new(row_count) { |index| Array.new(col_count) }
+    @galaxies = []
+  end
+
+  def row_count
+    @grid.size
+  end
+
+  def col_count
+    @grid.first.size
   end
 
   def fetch(row:, col:)
     @grid[row][col]
   end
 
-  def place(row:, col:, value:)
-    @grid[row][col] = value
+  def place(row:, col:, label:)
+    tile = Tile.new(row:, col:)
+    if label == "#" then
+      id = @galaxies.size
+      tile.id = id
+      @galaxies << tile
+    end
+    @grid[row][col] = tile
   end
 
   def [](row_coord)
@@ -47,7 +86,10 @@ class Cosmos
   def expand_rows(grid)
     empty_indexes = []
     grid.each.with_index do |row, index|
-      empty_indexes << index if row.all? { |c| c == "." }
+      if row.all? { |c| c.space? } then
+        # because adding a new index then bumps the index for the next insertion
+        empty_indexes << (index + empty_indexes.size)
+      end
     end
     empty_indexes.each { |i| grid.insert(i, grid[i].dup) }
     grid
@@ -65,15 +107,17 @@ cosmos    = Cosmos.new(row_count: row_count, col_count: col_count)
 
 chars.each.with_index do |row, row_coord|
   row.each.with_index do |label, col_coord|
-    cosmos.place(row: row_coord, col: col_coord, value: label)
+    cosmos.place(row: row_coord, col: col_coord, label: label)
   end
 end
 
 
 puts cosmos.to_s
+puts "#{cosmos.row_count} X #{cosmos.col_count}"
 cosmos.expand
 
 puts
 
 puts cosmos.to_s
+puts "#{cosmos.row_count} X #{cosmos.col_count}"
 
