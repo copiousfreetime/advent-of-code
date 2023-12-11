c74dccebf4298b87f0fb73c4f771ea59688a6510
copiousfreetime <jeremy@copiousfreetime.org>
2023-12-10 23:42:46 -0700

    rework algorithm to use width

--- 2023/11/answer.rb
+++ 2023/11/answer.rb
@@ -1,22 +1,21 @@
 #!/usr/bin/env ruby
 require 'bundler/inline'
 require 'debug'
-
-gemfile do
-  source 'https://rubygems.org'
-  gem 'pastel'
-  gem 'tty-cursor'
-end
+require 'optparse'
 
 class Tile
   attr_accessor :row
   attr_accessor :col
   attr_accessor :id
+  attr_accessor :width
+  attr_accessor :height
 
   def initialize(row:, col:, id: nil)
     @row = row
     @col = col
     @id = id
+    @width = 1
+    @height = 1
   end
 
   def galaxy?
@@ -34,7 +33,12 @@ class Tile
   end
 
   def to_s
-    galaxy? ? "#" : "."
+    return "#" if galaxy?
+    return "." if width == 1 && height == 1
+    return "o" if width > 1 && height == 1
+    return "o" if width == 1 && height > 1
+    return "*" if width > 1 && height > 1
+    return "?"
   end
 end
 
@@ -43,9 +47,10 @@ class Cosmos
   attr_reader :initial_col_count
   attr_reader :galaxies
 
-  def initialize(row_count:, col_count:)
+  def initialize(row_count:, col_count:, expansion_factor: 2)
     @initial_row_count = row_count
     @initial_col_count = col_count
+    @expansion_factor = expansion_factor
     @grid = Array.new(row_count) { |index| Array.new(col_count) }
     @galaxies = []
   end
@@ -59,11 +64,11 @@ class Cosmos
   end
 
   def row_count
-    @grid.size
+    @grid.map { |r| r[0].height }.sum
   end
 
   def col_count
-    @grid.first.size
+    @grid.first.map { |c| c.width }.sum
   end
 
   def fetch(row:, col:)
@@ -93,42 +98,55 @@ class Cosmos
   end
 
   def expand
-    @grid = expand_cols(expand_rows(@grid))
+    @grid = expand_cols(grid: expand_rows(grid: @grid))
     update_tile_coordinates
   end
 
   def update_tile_coordinates
-    @grid.each.with_index do |row, row_coord|
-      row.each.with_index do |tile, col_coord|
-        tile.row = row_coord
-        tile.col = col_coord
+    row_offset = 0
+
+    @grid.each do |row|
+      col_offset = 0
+
+      row.each do |tile|
+        tile.row = row_offset
+        tile.col = col_offset
+        col_offset += tile.width
       end
+
+      row_offset += row[0].height
     end
   end
 
   private
 
-  def expand_rows(grid)
-    empty_indexes = []
-    grid.each.with_index do |row, index|
+  def expand_rows(grid:, dimension: :height)
+    grid.each do |row|
       if row.all? { |c| c.space? } then
-        # because adding a new index then bumps the index for the next insertion
-        empty_indexes << (index + empty_indexes.size)
+        row.each { |c| c.send("#{dimension}=", @expansion_factor) }
       end
     end
-    empty_indexes.each { |i| grid.insert(i, grid[i].dup) }
     grid
   end
 
-  def expand_cols(grid)
-    expand_rows(grid.transpose).transpose
+  def expand_cols(grid:)
+    expand_rows(grid: grid.transpose, dimension: :width).transpose
   end
 end
+
+expansion_factor = 2
+OptionParser.new do |opts|
+  opts.banner = "Usage: answer.rb [options]"
+
+  opts.on("-eSIZE",  "--expansion=SIZE", "Sent Expansion Factor") do |f|
+    expansion_factor = f.to_i
+  end
+end.parse!
 
 chars     = ARGF.readlines.map(&:strip).map(&:chars)
 row_count = chars.size
 col_count = chars[0].length
-cosmos    = Cosmos.new(row_count: row_count, col_count: col_count)
+cosmos    = Cosmos.new(row_count: row_count, col_count: col_count, expansion_factor: expansion_factor)
 
 chars.each.with_index do |row, row_coord|
   row.each.with_index do |label, col_coord|
