69db7eb9df56f07feda37e74429f93d0dc1d6dd4
copiousfreetime <jeremy@copiousfreetime.org>
2023-12-29 09:38:03 -0700

    day 15 done

--- 2023/15/solution.rb
+++ 2023/15/solution.rb
@@ -10,7 +10,38 @@ def h(input)
   return value
 end
 
+boxes = Array.new(256) { |a| a = Hash.new }
+
 sequences = ARGF.read.chomp.split(",")
 
-result = sequences.map { |seq| h(seq) }.sum
-puts result
+sequences.each do |seq|
+  #puts "After \"#{seq}\""
+  if seq =~ /=/ then
+    label, focal = seq.split("=")
+
+    box_slot = h(label)
+    box = boxes[box_slot]
+    box[label] = focal.to_i
+  elsif seq =~ /-/ then
+    label, _ = seq.split("-")
+    box_slot = h(label)
+    box = boxes[box_slot]
+    box.delete(label)
+  else
+    raise "boom"
+  end
+end
+
+total = 0
+boxes.each.with_index do |b, i|
+  next if b.empty?
+  b_score = i + 1
+  b.to_a.each.with_index do |(label, focal), i2|
+    lens_score = b_score * (i2 + 1) * focal
+    puts "label: #{label} #{i+1} * #{(i2+1)} * #{focal} => #{lens_score}"
+    total += lens_score
+  end
+end
+
+puts total
+
